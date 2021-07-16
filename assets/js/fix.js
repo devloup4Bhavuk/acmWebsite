fbd6ba64e6718241204731b52b9fcdb466e30ae9
Akarsh Anand Sinha <sinhaakarsh64@gmail.com>
2021-07-16 19:00:10 +0530

    nav bar full fixed

--- assets/js/fix.js
+++ assets/js/fix.js
@@ -1,11 +1,22 @@
 
-document.getElementsByClassName('swiper-slide')[0].addEventListener("click", go);
-document.getElementsByClassName('close')[0].addEventListener("click", run);
+var xoxo = document.getElementsByClassName('swiper-slide');
+var coco = document.getElementsByClassName('close');
+for (let index = 0; index < 20; index++) {
+    xoxo[index].addEventListener("click", go);
+    coco[index].addEventListener("click", run);
+    
+}
+
+
 
 function go() {
     document.getElementsByClassName("navigation_button")[0].style.display = 'none';
+    document.getElementsByClassName("navigation_background")[0].style.display = 'none';
+
 }
 
 function run() {
     document.getElementsByClassName("navigation_button")[0].style.display = 'block';
+    document.getElementsByClassName("navigation_background")[0].style.display = 'block';
+
 }
